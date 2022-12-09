d78b107e921675af545333cf1f898223029a13c5
kiptirop <augustinetirop@gmail.com>
2022-12-09 10:07:29 -0800

    Advanced 2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,7 +4,6 @@
  * main - prints all possible different combinations of three digits
  *
  * Return: Always 0 (Success)
- *
  */
 
 int main(void)
@@ -38,4 +37,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-	
