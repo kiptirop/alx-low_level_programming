f5b85c854041ee28c69f060f6b11ab0f1bcc0b27
kiptirop <augustinetirop@gmail.com>
2022-12-13 08:33:12 -0800

    Putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,4 +16,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 void num(int r,int n);
 
-#end
+#endif
