2a833548e10072a0d89bfa94826f2e9b317b2ba0
kiptirop <augustinetirop@gmail.com>
2022-12-13 22:37:31 -0800

    Advanced 4

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even Valued Fibonacci sequence
+ *
+ * Return: Always (0)
+ *
+ */
+
+int main(void)
+{
+	int a = 0, b = 1, next = 0;
+	int sum = 0;
+
+	while (next < 4000000)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		if (next % 2 == 0)
+			sum += next;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
