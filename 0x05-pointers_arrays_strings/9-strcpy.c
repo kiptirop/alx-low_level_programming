d9ce36cea105b6f75e91d2c0d2b34bfec57fe57c
kiptirop <augustinetirop@gmail.com>
2022-12-19 02:53:32 -0800

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - function that copies the string pointed to by src
+ * @dest: pointer param
+ * @src: pointer param
+ * Return: 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; *(src + i) != '\0'; i++)
+	{
+		dest[i] = *(src + i);
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
