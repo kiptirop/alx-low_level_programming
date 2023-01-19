e90f093788e2d7a14f615c7e069d4e84608b3b32
kiptirop <augustinetirop@gmail.com>
2023-01-19 06:27:07 -0800

    Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - calls a function to act on array elements
+ * @array: name of the array
+ * @size: size of the passed array to function
+ * @action: the function that acts on the array elements
+ * Return: 0
+ */
+
+void array_iterator(int *array, size_t, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
